c15f6643d6ce70995b0fd27d57780efdfd701017
Nhlamulo-hl <ST10107399@rcconnect.edu.za>
2023-10-26 02:11:43 +0200

    A new Page was created (Allocation_Goods) for the Allocate Goods Branch

---
+++ Areas/Identity/Pages/Account/Allocate_Money.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GemFund.Areas.Identity.Pages.Account
+{
+    public class Allocate_MoneyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
